560a34f413104162e90dba48847b38343363f02f
Andriank0 <l.prystansky@gmail.com>
2018-11-01 03:13:18 -0700

    responsive

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,13 @@
 $(function() {
+  $('.burger, .overlay').click(function(){
+    $('.burger').toggleClass('clicked');
+    $('.overlay').toggleClass('show');
+    $('nav').toggleClass('show');
+    $('body').toggleClass('overflow');
+  });
 
   $('*[data-js="clip-it"]').ClipPath('25% 0%, 100% 0%, 75% 100%, 0% 100%');
+
   
 });
+
