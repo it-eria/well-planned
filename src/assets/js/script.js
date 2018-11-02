f8a0da9917242aaad2d94aedba608df237a53c7c
Andriank0 <l.prystansky@gmail.com>
2018-11-02 17:30:29 +0200

    add page pricing and made responsive

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -8,20 +8,17 @@ $(function() {
 
   $('*[data-js="clip-it"]').ClipPath('25% 0%, 100% 0%, 75% 100%, 0% 100%');
 
-  $('.my-select').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    arrows: true,
-    autoplaySpeed: 2000,
-    nextArrow:$('.canvass-btn-next'),
-    prevArrow:$('.canvass-btn-prew') 
-  });
-  
 });
 
-
-
+$('.my-select').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  arrows: true,
+  autoplaySpeed: 2000,
+  nextArrow:$('.canvass-btn-next'),
+  prevArrow:$('.canvass-btn-prew') 
+});
 
 
 
